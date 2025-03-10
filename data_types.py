d418d232c44c61a5c02733b7de4e0e8badd7404a
agwata <otienocee@gmail.com>
2025-03-10 06:31:51 +0300

    data_types

--- data_types.py
+++ data_types.py
@@ -1,4 +1,19 @@
 # STRING
+name = "John"
+color = "Red"
+email = "abc@abc.com"
+
 # INTEGER
+age = 36
+quantity = 2
+number_of_apples = 3
+
 # FLOAT
+pi = 3.14
+price = 2.99
+discount = 1.10
+
 # BOOLEAN
+is_active = True
+on_sale = False
+online = True
